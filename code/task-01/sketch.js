2343134b050f55a28534aa7f5bef302b50364755
areallbad <103072866+areallbad@users.noreply.github.com>
2022-04-06 18:00:20 +0200

    Update sketch.js

--- code/task-01/sketch.js
+++ code/task-01/sketch.js
@@ -7,5 +7,44 @@ function setup() {
 }
 
 function draw() {
-  background(100);
+  
+  background('#E8B338');
+
+  stroke(255);
+  strokeWeight(1.5);
+  
+  fill('#FF4AAC');
+  circle(200, 200, 300);
+  
+  fill('#E8434A');
+  circle(200, 200, 275);
+  
+  fill('#FF974A');
+  circle(200, 200, 235);
+  
+  fill('#E87143');
+  circle(200, 200, 190);
+  arc(
+    200, 200, 
+    190, 190,
+    Math.PI / 180 * 45,
+    Math.PI / 180 * 315,
+  );
+  
+  fill('#FF6C57');
+  circle(200, 200, 130);
+  
+  line(200, 200, 400, 400);
+  line(400, 0, 200, 200);
+
+  stroke(255, 0, 0);
+
+  arc(
+    200, 200, 
+    130, 130,
+    Math.PI / 180 * 45,
+    Math.PI / 180 * 315,
+  );
+
+
 }
